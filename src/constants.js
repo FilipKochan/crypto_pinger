f1c49266ebc5fb669d7a25be0560caebfd820808
FilipKochan <filda.koch@gmail.com>
2022-01-16 00:37:26 +0100

    test prettier

--- src/constants.js
+++ src/constants.js
@@ -6,4 +6,4 @@ export const TOKEN_1_CODE = 'coin'
 export const TOKEN_2_CODE = 'kdlaunch.token'
 export const DEF_LOWER_BOUND = 0.035
 export const DEF_UPPER_BOUND = 0.04
-export const DEF_INTERVAL = 30000
+export const DEF_INTERVAL = 30_000
