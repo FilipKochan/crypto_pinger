ccb2525f833bebdb2df6abf8c62f55de707bafdc
FilipKochan <filda.koch@gmail.com>
2022-01-16 13:36:20 +0100

    changed import to require

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,19 @@
-export const CHAIN_ID = '3'
-export const GAS_PRICE = 1e-8
-export const NETWORK = `https://api.chainweb.com/chainweb/0.0/mainnet01/chain/${CHAIN_ID}/pact`
+const CHAIN_ID = '3'
+const GAS_PRICE = 1e-8
+const NETWORK = `https://api.chainweb.com/chainweb/0.0/mainnet01/chain/${CHAIN_ID}/pact`
+const TOKEN_1_CODE = 'coin'
+const TOKEN_2_CODE = 'kdlaunch.token'
+const DEF_LOWER_BOUND = 0.035
+const DEF_UPPER_BOUND = 0.04
+const DEF_INTERVAL = 30_000
 
-export const TOKEN_1_CODE = 'coin'
-export const TOKEN_2_CODE = 'kdlaunch.token'
-export const DEF_LOWER_BOUND = 0.035
-export const DEF_UPPER_BOUND = 0.04
-export const DEF_INTERVAL = 30_000
+module.exports = {
+  CHAIN_ID,
+  GAS_PRICE,
+  NETWORK,
+  TOKEN_1_CODE,
+  TOKEN_2_CODE,
+  DEF_LOWER_BOUND,
+  DEF_UPPER_BOUND,
+  DEF_INTERVAL
+}
--- src/utils.js
+++ src/utils.js
@@ -1,21 +1,21 @@
-import Pact from 'pact-lang-api'
-import {
+const Pact = require('pact-lang-api')
+const {
   CHAIN_ID,
   GAS_PRICE,
   NETWORK,
   TOKEN_1_CODE,
   TOKEN_2_CODE
-} from './constants.js'
-import path from 'path'
+} = require('./constants.js')
+const path = require('path')
 
-export const creationTime = () => Math.round(new Date().getTime() / 1000) - 10
+const creationTime = () => Math.round(new Date().getTime() / 1000) - 10
 
 /**
  * Returns value after the specified parameter name in `process.argv`.
  * @param {string} paramName name of the desired parameter
  * @returns {string | undefined}
  */
-export const getParam = (paramName) => {
+const getParam = (paramName) => {
   const index = process.argv.findIndex((val) => val === paramName)
   if (index === -1) return
   return process.argv[index + 1]
@@ -26,7 +26,7 @@ export const getParam = (paramName) => {
  * @param {number} ratio
  * @returns {string}
  */
-export const formatRateInfo = (ratio) => {
+const formatRateInfo = (ratio) => {
   const date = new Date()
   return `${date.getDate()}/${
     date.getMonth() + 1
@@ -37,20 +37,20 @@ export const formatRateInfo = (ratio) => {
  * Gets the current rate between `KDA` and `KDL`.
  * @returns {Promise<number>}
  */
-export const getRate = async () => {
+const getRate = async () => {
   try {
     const res = await Pact.fetch.local(
       {
         pactCode: `
-              (use free.exchange)
-              (let*
-                (
-                  (p (get-pair ${TOKEN_1_CODE} ${TOKEN_2_CODE}))
-                  (reserveA (reserve-for p ${TOKEN_1_CODE}))
-                  (reserveB (reserve-for p ${TOKEN_2_CODE}))
-                  (totalBal (free.tokens.total-supply (free.exchange.get-pair-key ${TOKEN_1_CODE} ${TOKEN_2_CODE})))
-                )[totalBal reserveA reserveB])
-               `,
+        (use free.exchange)
+        (let*
+          (
+            (p (get-pair ${TOKEN_1_CODE} ${TOKEN_2_CODE}))
+            (reserveA (reserve-for p ${TOKEN_1_CODE}))
+            (reserveB (reserve-for p ${TOKEN_2_CODE}))
+            (totalBal (free.tokens.total-supply (free.exchange.get-pair-key ${TOKEN_1_CODE} ${TOKEN_2_CODE})))
+            )[totalBal reserveA reserveB])
+            `,
         meta: Pact.lang.mkMeta(
           '',
           CHAIN_ID,
@@ -69,17 +69,24 @@ export const getRate = async () => {
   }
 }
 
-export const consoleInfo = (lower, upper, interval) => {
+const consoleInfo = (lower, upper, interval) => {
   console.clear()
   console.log(
     `Using ${lower} as lower bound.
 Using ${upper} as upper bound.
-Getting new data every ${interval / 1000} seconds.
-`
+Getting new data every ${interval / 1000} seconds.`
   )
   console.log(
     '\x1b[32m%s\x1b[0m',
     `Logging rates into ${path.join(process.cwd(), 'log.txt')}`
   )
   console.log('\nYou can stop the app with CTRL-C...')
+}
+
+module.exports = {
+  creationTime,
+  getParam,
+  formatRateInfo,
+  getRate,
+  consoleInfo
 }
